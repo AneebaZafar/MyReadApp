7900bb3c19873554b4ee0f483bafcd6a360f2fa7
AneebaZafar <114745662+AneebaZafar@users.noreply.github.com>
2025-04-12 16:26:55 -0700

    Update settings.py

--- ReadApp/ritepad/settings.py
+++ ReadApp/ritepad/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$^*+tuamqs$&fj%=p3wphel(5i5uo0h!l8raw@=aa1nda1$*u*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
